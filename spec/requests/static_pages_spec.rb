54f4f2dd0bb14b2ad6f69e98ad21b70315907e6c
free <747549945@qq.com>
2013-11-02 15:32:34 +0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,8 +5,37 @@ describe "StaticPages" do
 
     describe "Home page" do
         it "应该有内容'Sample App'" do
-        	visit '/static_page/home'
+        	visit '/static_pages/home'
         	expect(page).to have_content('Sample App')
         end
+
+        it "should have the title 'Home'" do
+            visit '/static_pages/home'
+            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+        end
+    end
+
+    describe "Help page" do
+    	it "包含'Help'" do
+    		visit '/static_pages/help'
+    		expect(page).to have_content('Help')
+    	end
+
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		end
+    end
+
+    describe "About page" do
+    	it "包含'About'" do
+    		visit '/static_pages/about'
+    		expect(page).to have_content('About')
+    	end
+
+		it "should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+		end
     end
 end
