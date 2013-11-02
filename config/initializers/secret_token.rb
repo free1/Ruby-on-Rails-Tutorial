3ab25821c5864768dd5e49d2a99b7b1ba00e6570
free <747549945@qq.com>
2013-11-02 11:22:53 +0800

    安装所需gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'b3438fa156e4d77eb8eafeea21a12afe7e09eaebf1ca678735e29ea054a832f1e1615166721d0935b07948c4118e663adc5e58ff8ba18644eaff816bc7b4290a'
