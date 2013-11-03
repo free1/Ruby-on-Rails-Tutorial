134cccf60a4146b72abf5595a197ce5b4a15a423
free <747549945@qq.com>
2013-11-03 15:40:54 +0800

    测试帮助方法

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,7 +42,7 @@ describe "StaticPages" do
     describe "Contact page" do
         before { visit contact_path }
 
-        it { should have_content('Contact') }
+        it { should have_selector('h1', text: 'Contact') }
         it { should have_title(full_title('Contact')) }
 
         # it "包含'Contact'" do
@@ -67,5 +67,20 @@ describe "StaticPages" do
 		# it "should have the title 'About Us'" do
 		# 	expect(page).to have_title("#{title_name} | About Us")
 		# end
+    end
+
+    it "should have the right links on the layout" do
+        visit root_path
+        click_link "About"
+        expect(page).to have_title(full_title('About Us'))
+        click_link "Help"
+        expect(page).to have_title(full_title('Help'))
+        click_link "Contact"
+        expect(page).to have_title(full_title('Contact'))
+        click_link "Home"
+        click_link "Sign up now!"
+        expect(page).to have_title(full_title('Sign up'))
+        click_link "sample app"
+        expect(page).to have_title(full_title(''))
     end
 end
